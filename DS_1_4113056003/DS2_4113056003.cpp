b7c5224f54eec3bce7fbc09e6c5059b79c4068cf
tangkaiyi <tangkaiyi0326@gmail.com>
2025-10-16 21:08:32 +0800

    Initial commit

---
+++ DS_1_4113056003/DS2_4113056003.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+
+
+void slove_case(){
+    string s;
+    cin >> s;
+    int n = s.length();
+    vector<int>dp(n+1,n+1);
+    dp[0] = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            bool pal = true;
+            int l = j,r = i;
+            while(l<r){
+                if(s[l] != s[r]){
+                    pal = false;
+                    break;
+                }
+                l++;
+                r--;
+            }
+            if(pal){
+                dp[i+1] = min(dp[j]+1,dp[i+1]);
+            }
+        }
+    }
+    cout << dp[n] << endl;
+}
+
+int main(){
+    freopen("testcase2.txt","r",stdin);
+    freopen("output2.txt","w",stdout);
+
+    int n;
+    cin >> n;
+    while(n--){
+        slove_case();
+    }
+}
---
+++ DS_1_4113056003/DS3_4113056003.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+
+#define MAX_INT 2147483647
+
+using namespace std;
+
+void slove_case(){
+    string s;
+    cin >> s;
+    int len = s.length();
+    vector<long long> dp(len + 1, 0);
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j <= i; j++) {
+            string subs = s.substr(j, i - j + 1);
+            if (subs.empty() || (subs[0] == '0' && subs.length() > 1)) continue;
+            long long subint = 0;
+            try {
+                subint = stoll(subs);
+            } catch (...) {
+                continue;
+            }
+            if (subint > MAX_INT) continue;
+            dp[i + 1] = max(dp[i + 1], dp[j] + subint);
+        }
+    }
+    cout << dp[len] << endl;
+}
+
+int main(){
+    freopen("testcase3.txt", "r", stdin);
+    freopen("output3.txt", "w", stdout);
+    int t;
+    cin >> t;
+    while(t--){
+        slove_case();
+    }
+}
