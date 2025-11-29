16fa832b2a3e9633d8bad14b4a11d645a3d56267
tangkaiyi <tangkaiyi0326@gmail.com>
2025-11-29 12:02:15 +0800

    shit

--- DS_3_4113056003/DS4_4113056003.cpp
+++ DS_3_4113056003/DS4_4113056003.cpp
@@ -1,123 +1,123 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
-#include <cmath>
 
 using namespace std;
 
-const int MAXN = 500001;
+const int MAXN = 500005;
 
-struct Node {
+struct TreeNode {
     int val;
-    Node *left, *right;
-    Node(int v) : val(v), left(NULL), right(NULL) {}
+    TreeNode *left, *right;
+    TreeNode(int v) : val(v), left(NULL), right(NULL) {}
 };
 
-int inPos[MAXN];
-int levelPos[MAXN];
-int st[MAXN][20];
-int logs[MAXN];
-int subtreeSize[MAXN];
-Node* nodePtrs[MAXN];
+struct Task {
+    TreeNode* parent;
+    bool isLeft;
+    int l, r;
+};
 
-void buildST(int n, const vector<int>& inorder) {
-    logs[1] = 0;
-    for (int i = 2; i <= n; i++) logs[i] = logs[i/2] + 1;
-    
-    for (int i = 0; i < n; i++) {
-        st[i][0] = i;
-    }
-    
-    for (int j = 1; j < 20; j++) {
-        for (int i = 0; i + (1 << j) <= n; i++) {
-            int idx1 = st[i][j-1];
-            int idx2 = st[i + (1 << (j-1))][j-1];
-            
-            if (levelPos[inorder[idx1]] < levelPos[inorder[idx2]])
-                st[i][j] = idx1;
-            else
-                st[i][j] = idx2;
+int inorderPos[MAXN];
+int subtreeSize[MAXN];
+TreeNode* targetNode = NULL;
+
+int countSize(TreeNode* node) {
+    if (!node) return 0;
+    int s = 1 + countSize(node->left) + countSize(node->right);
+    subtreeSize[node->val] = s;
+    return s;
+}
+
+TreeNode* buildTree(int n, const vector<int>& levelOrder, int targetX) {
+    if (n == 0) return NULL;
+
+    TreeNode* root = new TreeNode(levelOrder[0]);
+    if (root->val == targetX) targetNode = root;
+
+    queue<Task> q;
+    int rootIdx = inorderPos[root->val];
+
+    if (0 <= rootIdx - 1)
+        q.push({root, true, 0, rootIdx - 1});
+    if (rootIdx + 1 <= n - 1)
+        q.push({root, false, rootIdx + 1, n - 1});
+
+    int currentLevelIdx = 1;
+
+    while (!q.empty() && currentLevelIdx < n) {
+        Task task = q.front();
+        q.pop();
+
+        int val = levelOrder[currentLevelIdx++];
+        TreeNode* newNode = new TreeNode(val);
+
+        if (val == targetX) targetNode = newNode;
+
+        if (task.isLeft) {
+            task.parent->left = newNode;
+        } else {
+            task.parent->right = newNode;
+        }
+
+        int currIdx = inorderPos[val];
+
+        if (task.l <= currIdx - 1) {
+            q.push({newNode, true, task.l, currIdx - 1});
+        }
+        if (currIdx + 1 <= task.r) {
+            q.push({newNode, false, currIdx + 1, task.r});
         }
     }
-}
-
-int queryST(int L, int R, const vector<int>& inorder) {
-    int j = logs[R - L + 1];
-    int idx1 = st[L][j];
-    int idx2 = st[R - (1 << j) + 1][j];
-    if (levelPos[inorder[idx1]] < levelPos[inorder[idx2]]) return idx1;
-    else return idx2;
-}
-
-Node* buildTree(int L, int R, const vector<int>& inorder) {
-    if (L > R) return NULL;
-    
-    int rootIdx = queryST(L, R, inorder);
-    int rootVal = inorder[rootIdx];
-    
-    Node* root = new Node(rootVal);
-    nodePtrs[rootVal] = root;
-    
-    root->left = buildTree(L, rootIdx - 1, inorder);
-    root->right = buildTree(rootIdx + 1, R, inorder);
-    
     return root;
 }
 
-int countSize(Node* u) {
-    if (!u) return 0;
-    int s = 1 + countSize(u->left) + countSize(u->right);
-    subtreeSize[u->val] = s;
-    return s;
+void solve_case() {
+    int targetX, n;
+    if (!(cin >> targetX >> n)) return;
+
+    targetNode = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int val;
+        cin >> val;
+        inorderPos[val] = i;
+    }
+
+    vector<int> levelOrder(n);
+    for (int i = 0; i < n; i++) {
+        cin >> levelOrder[i];
+    }
+
+    TreeNode* root = buildTree(n, levelOrder, targetX);
+    countSize(root);
+
+    bool canWin = false;
+    if (targetNode) {
+        int leftSize = (targetNode->left) ? subtreeSize[targetNode->left->val] : 0;
+        int rightSize = (targetNode->right) ? subtreeSize[targetNode->right->val] : 0;
+        int parentSize = n - subtreeSize[targetX];
+
+        if (leftSize > n / 2 || rightSize > n / 2 || parentSize > n / 2) {
+            canWin = true;
+        }
+    }
+
+    cout << (canWin ? "1" : "0") << "\n";
 }
 
 int main() {
     if(!freopen("testcase4.txt","r",stdin)) return -1;
     if(!freopen("output4.txt","w",stdout)) return -1;
-    
+
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    int T;
-    if (cin >> T) {
-        while (T--) {
-            int targetX;
-            cin >> targetX;
-            
-            int n;
-            cin >> n;
-            
-            vector<int> inorder(n);
-            for(int i=0; i<n; ++i) {
-                cin >> inorder[i];
-                inPos[inorder[i]] = i;
-            }
-            
-            vector<int> levelOrder(n);
-            for(int i=0; i<n; ++i) {
-                cin >> levelOrder[i];
-                levelPos[levelOrder[i]] = i;
-            }
-            
-            buildST(n, inorder);
-            Node* root = buildTree(0, n-1, inorder);
-            
-            countSize(root);
-            
-            bool canWin = false;
-            Node* xNode = nodePtrs[targetX];
-            
-            if (xNode) {
-                int lSize = (xNode->left) ? subtreeSize[xNode->left->val] : 0;
-                int rSize = (xNode->right) ? subtreeSize[xNode->right->val] : 0;
-                int pSize = n - subtreeSize[targetX];
-                
-                if (lSize > n/2 || rSize > n/2 || pSize > n/2) {
-                    canWin = true;
-                }
-            }
-            
-            cout << (canWin ? "1" : "0") << "\n";
+
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve_case();
         }
     }
     return 0;
